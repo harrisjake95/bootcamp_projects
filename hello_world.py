9ecf021542b98e81535192ac5fbc579c1c617e63
harrisjake95 <127328924+harrisjake95@users.noreply.github.com>
2023-06-24 10:52:33 +0100

    Add files via upload
    Task 1

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#variable for user to enter their name
+name = input("Please enter your name")
+
+#printing the entered name
+print(name)
+
+#variable for user to enter their age
+age = input("Please enter your age")
+
+#printing the entered age
+print(age)
+
+#printing the required string
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,22 @@
+#variables for items for user to order
+item1 = "Macaroni Cheese"
+item2 = "Tomato Soup"
+item3 = "Cheese on Toast"
+
+#printing menu for user to choose from
+print("Please see the below menu and choose your favourite items")
+print(item1)
+print(item2)
+print(item3)
+
+#input variables for user to enter their order
+order = input("Please enter your favourite item: ")
+order1 = input("Please enter your second favourite item: ")
+order2 = input ("Please enter your third favourite item: ")
+
+#printing the user's entered order confirmation
+print("Order Confirmation! You have ordered: ")
+print(order)
+print(order1)
+print(order2)
+
