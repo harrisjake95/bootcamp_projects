14ad24c6ee6a2c5262b88a1ea17d9e3d0c98e0f2
harrisjake95 <127328924+harrisjake95@users.noreply.github.com>
2023-06-24 16:23:13 +0100

    Add files via upload

---
+++ Task 08/task3.py
@@ -0,0 +1,7 @@
+#variable for my required stars output
+stars = "*"
+print(stars)
+#for loop to produce my original variable plus one another 4 times
+for i in range(4):
+    stars = stars +"*"
+    print(stars)
