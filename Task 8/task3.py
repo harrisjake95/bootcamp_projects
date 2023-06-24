2a813f5da19c1a37894ce777389bcfa3a6c520c0
harrisjake95 <127328924+harrisjake95@users.noreply.github.com>
2023-06-24 16:19:02 +0100

    Delete Task 8 directory

--- Task 8/task3.py
+++
@@ -1,7 +0,0 @@
-#variable for my required stars output
-stars = "*"
-print(stars)
-#for loop to produce my original variable plus one another 4 times
-for i in range(4):
-    stars = stars +"*"
-    print(stars)
